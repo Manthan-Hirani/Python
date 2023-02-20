de5e657764b9a63da98f261be0350da3396e8946
abc.xyz <abc.xyz@gmail.com>
2023-02-20 17:02:02 +0530

    Day 1 file 1 Completed

---
+++ Projects/Day-1/01.py
@@ -0,0 +1,15 @@
+str = "Python Developer"
+str = list(str)
+
+# Using While Loop
+
+# i = 0
+# while(i<len(str)):
+#     print(str[i],end=", ")
+#     i+=2
+
+# Using For Loop
+
+for i in range(0,len(str),2):
+    print(str[i],end=", ")
+        
